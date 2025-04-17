1994cb5e586d9da8430762526d5276fb94eedb99
EunHyeok <secun77@naver.com>
2025-04-17 11:46:51 +0900

    초기 커밋

---
+++ src/main/java/com/study/springcore/SpringCoreStudy2504Application.java
@@ -0,0 +1,13 @@
+package com.study.springcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreStudy2504Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCoreStudy2504Application.class, args);
+    }
+
+}
